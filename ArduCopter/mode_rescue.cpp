77711a32b030f901f0c0675735e6241ecb2e1953
PavanMeesala <pavanmeesala0@gmail.com>
2026-06-08 16:32:51 +0530

    changed rescue mode code to handle messages

--- ArduCopter/mode_rescue.cpp
+++ ArduCopter/mode_rescue.cpp
@@ -4,17 +4,15 @@
 
 bool ModeRescue::init(bool ignore_checks)
 {
-    // Reset waypoint state on every mode entry
-    _wp_count      = 0;
+    _wp_count       = 0;
     _expected_count = 0;
-    _current_idx   = 0;
+    _current_idx    = 0;
 
-    // Call parent guided mode init
     return ModeGuided::init(ignore_checks);
 }
 
 // ---------------------------------------------------------------------------
-// Called by GCS_MAVLink when a RESCUE_WP packet arrives.
+// Receive RESCUE_WP from GCS
 // ---------------------------------------------------------------------------
 void ModeRescue::handle_rescue_wp(uint16_t seq, uint16_t total_count,
                                    int32_t lat_degE7, int32_t lon_degE7)
@@ -29,11 +27,10 @@ void ModeRescue::handle_rescue_wp(uint16_t seq, uint16_t total_count,
     _expected_count = total_count;
 
     Location wp{};
-    wp.lat              = lat_degE7;
-    wp.lng              = lon_degE7;
-    wp.alt              = 0;
-    wp.relative_alt     = false;
-
+    wp.lat          = lat_degE7;
+    wp.lng          = lon_degE7;
+    wp.alt          = 0;
+    wp.relative_alt = false;
     _waypoints[seq] = wp;
 
     if (static_cast<uint16_t>(seq + 1) > _wp_count) {
@@ -44,22 +41,42 @@ void ModeRescue::handle_rescue_wp(uint16_t seq, uint16_t total_count,
         gcs().send_text(MAV_SEVERITY_INFO,
                         "RESCUE_WP: all %u waypoints received", _wp_count);
 
-        // Immediately send the first waypoint to guided mode navigation
-        if (_wp_count > 0) {
-            _current_idx = 0;
-            set_destination(_waypoints[_current_idx]);
+        // Forward all waypoints back to all GCS channels
+        for (uint8_t i = 0; i < _wp_count; i++) {
+            for (uint8_t c = 0; c < gcs().num_gcs(); c++) {
+                mavlink_msg_rescue_wp_send(
+                    gcs().chan(c)->get_chan(),
+                    _wp_count,
+                    i,
+                    _waypoints[i].lat,
+                    _waypoints[i].lng
+                );
+            }
         }
-    }
-}
-void ModeRescue::handle_user_wp_reached(uint16_t wp_index, uint8_t reached)
-{
-    if (reached) {
-        gcs().send_text(MAV_SEVERITY_INFO,
-                        "Waypoint %u reached", wp_index);
-    } else {
-        gcs().send_text(MAV_SEVERITY_INFO,
-                        "Waypoint %u not reached", wp_index);
+
+        // if (_wp_count > 0) {
+        //     _current_idx = 0;
+        //     set_destination(_waypoints[_current_idx]);
+        // }
     }
 }
 
-#endif  // MODE_RESCUE_ENABLED
+void ModeRescue::handle_user_wp_reached(uint16_t wp_index, uint8_t reached)
+{
+    if (reached) {
+        gcs().send_text(MAV_SEVERITY_INFO, "Waypoint %u reached", wp_index);
+    } else {
+        gcs().send_text(MAV_SEVERITY_INFO, "Waypoint %u not reached", wp_index);
+    }
+
+    // Forward structured MAVLink message back to all GCS channels
+    for (uint8_t c = 0; c < gcs().num_gcs(); c++) {
+        mavlink_msg_user_wp_reached_send(
+            gcs().chan(c)->get_chan(),
+            wp_index,
+            reached
+        );
+    }
+}
+
+#endif // MODE_RESCUE_ENABLED
